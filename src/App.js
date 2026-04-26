de6b792d87c388be2ef61b67c81ea58a4de9bdbf
Hannah Qi <hannahqi314@gmail.com>
2026-04-26 17:14:18 -0400

    deployment setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FullPage from "./pages/FullPage";
 import MinisPage from "./pages/MinisPage";
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<FullPage />} />
         <Route path="/minis" element={<MinisPage />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
